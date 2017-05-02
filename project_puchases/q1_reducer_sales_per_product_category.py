c5f9faadf24896fe6575c46fb5abb1668752a93b
linxinzhe <linxinzhechn@icloud.com>
2017-05-02 12:42:33 -0400

    feat(q1): map reduce result

--- project_puchases/q1_reducer_sales_per_product_category.py
+++ project_puchases/q1_reducer_sales_per_product_category.py
@@ -5,17 +5,24 @@ import sys
 salesTotal = 0
 oldKey = None
 
+# Loop around the data
+# It will be in the format key\tval
+# Where key is the store name, val is the sale amount
+#
+# All the sales for a particular store will be presented,
+# then the key will change and we'll be dealing with the next store
+
 for line in sys.stdin:
     data_mapped = line.strip().split("\t")
     if len(data_mapped) != 2:
-        # calm down and carry on
+        # Something has gone wrong. Skip this line.
         continue
 
     thisKey, thisSale = data_mapped
 
     if oldKey and oldKey != thisKey:
         print oldKey, "\t", salesTotal
-        oldKey = thisKey
+        oldKey = thisKey;
         salesTotal = 0
 
     oldKey = thisKey
@@ -23,3 +30,4 @@ for line in sys.stdin:
 
 if oldKey != None:
     print oldKey, "\t", salesTotal
+
