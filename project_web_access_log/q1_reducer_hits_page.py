bbbb7a5695d8ab14892321ab23eac4e92852fd43
林欣哲 <linxinzhe@Lins-MacBook-Pro.local>
2017-05-03 11:20:44 +0800

    fix(access): add test block

--- project_web_access_log/q1_reducer_hits_page.py
+++ project_web_access_log/q1_reducer_hits_page.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python
-
 import sys
 
-HIT_PAGE = "/assets/js/the-associates.js"
+# only work for test
+if __name__ == "__main__":
+    a = """
+/
+/favicon.ico
+/
+/assets/js/lowpro.js
+/assets/css/reset.css
+/assets/css/960.css
+/assets/css/the-associates.css
+/assets/js/the-associates.js
+/assets/js/lightbox.js
+/assets/img/search-button.gif
+    """
+    import StringIO
+
+    stringio = StringIO.StringIO(a)
+    sys.stdin = stringio
+
+HIT_PAGE = "/"
 hit_page_count = 0
 for line in sys.stdin:
-    page = line
+    page = line.strip()  # get rid of "\n"
     if page:
         if HIT_PAGE == page:
             hit_page_count += 1
