12a9946043d3f7772ea2bd0a9b3cbf73198bbbd4
林欣哲 <linxinzhe@Lins-MacBook-Pro.local>
2017-05-03 11:27:21 +0800

    fix(access): HIT_PAGE

--- project_web_access_log/q1_reducer_hits_page.py
+++ project_web_access_log/q1_reducer_hits_page.py
@@ -1,26 +1,7 @@
 #!/usr/bin/python
 import sys
 
-# only work for test
-if __name__ == "__main__":
-    a = """
-/
-/favicon.ico
-/
-/assets/js/lowpro.js
-/assets/css/reset.css
-/assets/css/960.css
-/assets/css/the-associates.css
-/assets/js/the-associates.js
-/assets/js/lightbox.js
-/assets/img/search-button.gif
-    """
-    import StringIO
-
-    stringio = StringIO.StringIO(a)
-    sys.stdin = stringio
-
-HIT_PAGE = "/"
+HIT_PAGE = "/assets/js/the-associates.js"
 hit_page_count = 0
 for line in sys.stdin:
     page = line.strip()  # get rid of "\n"
