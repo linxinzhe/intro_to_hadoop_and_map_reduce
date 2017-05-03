2a15e2fda2c236fd1e1f6224369ffd7109912064
林欣哲 <linxinzhe@Lins-MacBook-Pro.local>
2017-05-03 14:08:54 +0800

    feat(access): error log handle

--- project_web_access_log/q2_reducer_hits_ip.py
+++ project_web_access_log/q2_reducer_hits_ip.py
@@ -1,20 +1,39 @@
 #!/usr/bin/python
 # above is necessary for hadoop streaming
 
-import sys
 
-totalCount = 0
-HIT_IP = "10.99.99.186"
+def setup_call_andCloseOut():
+    totalCount = 0
+    HIT_IP = "10.99.99.186"
 
-for line in sys.stdin:
-    data_mapped = line.strip().split("\t")
-    if len(data_mapped) != 2:
-        # Something has gone wrong. Skip this line.
-        continue
+    for line in sys.stdin:
+        data_mapped = line.strip().split("\t")
+        if len(data_mapped) != 2:
+            # Something has gone wrong. Skip this line.
+            continue
 
-    thisKey, thisCount = data_mapped
+        thisKey, thisCount = data_mapped
 
-    if thisKey == HIT_IP:
-        totalCount += int(thisCount)
+        if thisKey == HIT_IP:
+            totalCount += int(thisCount)
 
-print "Hit IP:", HIT_IP, " Count:", totalCount
+    print "Hit IP:", HIT_IP, " Count:", totalCount
+
+
+if __name__ == '__main__':
+
+    try:
+        setup_call_andCloseOut()
+    except:
+        import sys, traceback, StringIO
+
+        fakeeWriteable = StringIO.StringIO()
+
+        traceback.print_exc(None, file=fakeeWriteable)
+        msg = ""
+        msg += "------------------------------------------------------\n"
+        msg += "----theTraceback: -----------\n"
+        msg += fakeeWriteable.getvalue() + "\n"
+        msg += "------------------------------------------------------\n"
+
+        sys.stderr.write(msg)
